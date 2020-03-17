984a8d309c2f3adca123081baf6164ae92366ced
stefan hranek <hranekstefan@gmail.com>
2020-03-16 19:41:50 -0700

    Set up HTML skeleton and text for About page

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <div>
+                        <img src="" alt=""/>
+                        <p>EXPLORE AND CREATE</p>
+                    </div>
+                </section>
+                <section>
+                    <h3>ABOUT</h3>
+                    <h1>We create brands,<br/>build products, and<br/>marketing thing</h1>
+                    <p>We're a team of design practitioners at Stefco who transform organizations through education</p>
+                    <button>Learn About Us</button>
+                </section>
+            </div>
+        )
+    }
+}
