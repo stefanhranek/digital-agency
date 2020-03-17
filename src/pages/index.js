a0a84fdde4a12282624378cde989c4120a2ddd94
stefan hranek <hranekstefan@gmail.com>
2020-03-16 22:09:21 -0700

    Installed sass & set up file structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import './../styles/App.scss';
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Home from "./Home"
 import About from "./About"
 import Services from "./Services"
+import Projects from "./Projects"
+import Blog from "./Blog"
+import Contact from "./Contact"
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +18,9 @@ const IndexPage = () => (
     <Home></Home>
     <About></About>
     <Services></Services>
+    <Projects></Projects>
+    <Blog></Blog>
+    <Contact></Contact>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
