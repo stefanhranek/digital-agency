e83da8d3ad74944a0664a7d7597ad4d531a7a984
stefan hranek <hranekstefan@gmail.com>
2020-03-16 21:19:45 -0700

    Set up html structure & text for Blog page

---
+++ src/pages/Blog.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Blog extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <img src="" alt=""/>
+                </section>
+                <section>
+                    <p>"It was an amazing experience sharing and learning alongside wonderful people in our industry. The great exchange, energy, and valuable lessons will surely be extended to my entire team at Apple."</p>
+                    <article>
+                        <div>
+                            <h3>Stefan Hranek</h3>
+                            <h4>LEAD UX DESIGNER, APPLE</h4>
+                        </div>
+                        <button>-></button>
+                    </article>
+                </section>
+            </div>
+        )
+    }
+}
