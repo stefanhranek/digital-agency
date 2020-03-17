2d6fcac2e626486936b2fbfdb297e20065889b07
stefan hranek <hranekstefan@gmail.com>
2020-03-16 18:44:02 -0700

    Home page structure and text set

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,34 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                <div>
+                    <section>
+                        <p>DIGITAL AGENCY</p>
+                        <h1>We Bring<br/>Ideas to Life</h1>
+                        <p>We're a team of designers and brand strategists who create beautiful websites</p>
+                        <button>Explore projects</button>
+                    </section>
+                    <section>
+                        <div>
+                            <article>Research</article>
+                            <article>UI Design</article>
+                            <article>Analysis</article>
+                            <img src="" alt=""/>
+                            <p>WE WORK WITH PASSION!</p>
+                        </div>
+                    </section>
+                </div>
+                <div>
+                    <ul>
+                        <li>Google</li>
+                        <li>Adobe</li>
+                        <li>PayPal</li>
+                        <li>Artesel</li>
+                        <li>Bloom</li>
+                    </ul>
+                </div>
+            </>
         )
     }
 }
