4e6c1080b98e8981ba8b951794724fb880e5a53d
stefan hranek <hranekstefan@gmail.com>
2020-03-16 18:17:43 -0700

    Filled out text & html skeleton for Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>stefco</h1>
+                <ul>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Work</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
+                <button>Start a Project</button>
+            </div>
+        )
+    }
+}
