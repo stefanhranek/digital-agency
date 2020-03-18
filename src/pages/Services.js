006b7e6e69ec386b567dffcfb6f292bf164a8d7b
stefan hranek <hranekstefan@gmail.com>
2020-03-17 22:20:42 -0700

    Added arrow button image to Service page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import IconService from '../components/IconService'
 
 export default class Services extends Component {
     render() {
@@ -9,13 +10,32 @@ export default class Services extends Component {
                     <p className="subtitle">SERVICES</p>
                     <h1 className="title">Designed for today<br/>and built for tomorrow</h1>
                 </section>
-                <section>
-                    <article>Brand Identity</article>
-                    <article>Product Design</article>
-                    <article>UI/UX Design</article>
-                    <article>Web Design</article>
-                    <article>App Design</article>
-                    <article>Development</article>
+                <section className="serviceTabContainer">
+                    <article className="serviceTab">
+                        <img src="" alt=""/>
+                        <p>Brand Identity</p>
+                        <IconService/>
+                    </article>
+                    <article className="serviceTab">
+                        <p>Product Design</p>
+                        <IconService/>
+                    </article>
+                    <article className="serviceTab">
+                        <p>UX/UI Design</p>
+                        <IconService/>
+                    </article>
+                    <article className="serviceTab">
+                        <p>Web Design</p>
+                        <IconService/>
+                    </article>
+                    <article className="serviceTab">
+                        <p>App Design</p>
+                        <IconService/>
+                    </article>
+                    <article className="serviceTab">
+                        <p>Development</p>
+                        <IconService/>
+                    </article>
                 </section>
             </div>
         )
