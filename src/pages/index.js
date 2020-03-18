306afc5d5e55e52f649d25f4131b2163f7aeb612
stefan hranek <hranekstefan@gmail.com>
2020-03-18 02:27:13 -0700

    Cleaned up the index file a bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import './../styles/App.scss';
+import "./../styles/App.scss"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -17,18 +17,15 @@ import Navbar from "./../components/Navbar"
 const IndexPage = () => (
   <div>
     <SEO title="Home" />
-    <Navbar></Navbar>
-    <Home></Home>
-    <Companies/>
-    <About></About>
-    <Services></Services>
-    <Projects></Projects>
-    <Blog></Blog>
-    <Contact></Contact>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Navbar />
+    <Home />
+    <Companies />
+    <About />
+    <Services />
+    <Projects />
+    <Blog />
+    <Contact />
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </div>
 )
 
