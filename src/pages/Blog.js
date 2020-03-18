3de5772b8085615f8f126e47e194d4f2fadc8814
stefan hranek <hranekstefan@gmail.com>
2020-03-18 01:37:38 -0700

    MVP & basic styling done for Blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import homeImage from "../images/homeImage.svg";
 
 export default class Blog extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <div className="blog">
+        <section className="imageContainer">
+          <div className="homeImage">
+            <img src={homeImage} alt="" />
+            <p className="sidewaysText">EXPLORE AND CREATE!</p>
+          </div>
+        </section>
+        <section>
+          <p className="title">
+            "It was an amazing experience
+            <br />
+            sharing and learning alongside
+            <br />
+            wonderful people from our
+            <br />
+            industry. The great exchange,
+            <br />
+            energy, and valuable lessons
+            <br />
+            will absolutely be extended to
+            <br />
+            my entire team at Apple."
+          </p>
+          <article>
             <div>
-                <section>
-                    <img src="" alt=""/>
-                </section>
-                <section>
-                    <p className="title">"It was an amazing experience sharing and learning alongside wonderful people in our industry. The great exchange, energy, and valuable lessons will surely be extended to my entire team at Apple."</p>
-                    <article>
-                        <div>
-                            <h3>Stefan Hranek</h3>
-                            <h4>LEAD UX DESIGNER, APPLE</h4>
-                        </div>
-                        <button>-></button>
-                    </article>
-                </section>
+              <h3 className="name">Stefan Hranek</h3>
+              <h4>LEAD UX DESIGNER, APPLE</h4>
             </div>
-        )
-    }
+            <button>-></button>
+          </article>
+        </section>
+      </div>
+    )
+  }
 }
