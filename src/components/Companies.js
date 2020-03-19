598af84361df65b5dca4c7e3e6f9d5a32565663b
stefan hranek <hranekstefan@gmail.com>
2020-03-18 18:08:58 -0700

    Company logos positioned

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,20 +1,30 @@
-import React, { Component } from "react";
-import google from '../images/google.png';
-import adobe from '../images/adobe.png';
-import paypal from '../images/paypal.png';
-import artesal from '../images/artesal.png';
-import bloom from '../images/bloom.png';
+import React, { Component } from "react"
+import google from "../images/google.png"
+import adobe from "../images/adobe.png"
+import paypal from "../images/paypal.png"
+import artesal from "../images/artesal.png"
+import bloom from "../images/bloom.png"
 
 export default class Companies extends Component {
   render() {
     return (
       <div className="companiesSection">
         <ul>
-          <li><img src={google} alt="google"/></li>
-          <li><img src={adobe} alt="adobe"/></li>
-          <li><img src={paypal} alt="paypal"/></li>
-          <li><img src={artesal} alt="artesal"/></li>
-          <li><img src={bloom} alt="bloom"/></li>
+          <li>
+            <img src={google} alt="google" />
+          </li>
+          <li>
+            <img src={adobe} alt="adobe" />
+          </li>
+          <li>
+            <img id="paypal" src={paypal} alt="paypal" />
+          </li>
+          <li>
+            <img id="artesal" src={artesal} alt="artesal" />
+          </li>
+          <li>
+            <img src={bloom} alt="bloom" />
+          </li>
         </ul>
       </div>
     )
