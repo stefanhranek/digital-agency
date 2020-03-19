5e442b08f1780a3374ff19dd60427a7d1d93ad01
stefan hranek <hranekstefan@gmail.com>
2020-03-18 17:52:49 -0700

    Added custom logos for companies

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import google from '../images/google.png';
+import adobe from '../images/adobe.png';
+import paypal from '../images/paypal.png';
+import artesal from '../images/artesal.png';
+import bloom from '../images/bloom.png';
 
 export default class Companies extends Component {
   render() {
     return (
       <div className="companiesSection">
         <ul>
-          <li>Google</li>
-          <li>Adobe</li>
-          <li>PayPal</li>
-          <li>Artesel</li>
-          <li>Bloom</li>
+          <li><img src={google} alt="google"/></li>
+          <li><img src={adobe} alt="adobe"/></li>
+          <li><img src={paypal} alt="paypal"/></li>
+          <li><img src={artesal} alt="artesal"/></li>
+          <li><img src={bloom} alt="bloom"/></li>
         </ul>
       </div>
     )
