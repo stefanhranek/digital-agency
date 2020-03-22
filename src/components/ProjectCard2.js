29609a0921bfe8306b5424af5d153bcb442511cb
stefan hranek <hranekstefan@gmail.com>
2020-03-21 17:01:02 -0700

    Project card styled for mobile view

--- src/components/ProjectCard2.js
+++ src/components/ProjectCard2.js
@@ -1,24 +1,19 @@
-import React, { Component } from "react";
-import rightArrow from "../images/arrowRightBlack.svg";
+import React, { Component } from "react"
+import rightArrow from "../images/arrowRightBlack.svg"
 import projectCardImage from "../images/projectCard.png"
 
 export default class ProjectCard2 extends Component {
   render() {
     return (
       <div className="projectCard p2">
-        <section className="projectImageContainer"/>
+        <section className="projectImageContainer" />
         <section className="left">
           <p className="subtitle">WEB DESIGN</p>
           <h3 className="title">
-            Spotcheck provides
-            <br />
-            skaters a map
-            <br /> to the best spots
+            Spotcheck provides skaters a map to the best spots
           </h3>
           <p className="description">
-            Explore, discover, create, share.
-            <br />
-            Lift up the skateboard community!
+            Explore, discover, create, share. Lift up the skateboard community!
           </p>
           <button>
             <img src={rightArrow} alt="right arrow" />
@@ -26,7 +21,7 @@ export default class ProjectCard2 extends Component {
           </button>
         </section>
         <section className="right">
-            <img src={projectCardImage} alt="app screenshot" />
+          <img src={projectCardImage} alt="app screenshot" />
         </section>
       </div>
     )
